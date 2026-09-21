11e04c6a4ab6aa9308fe4f7f136b4cbcdb471ec1
Fasching Markus Harald <fasching@cosmopc24.intranet.epfl.ch>
2026-09-21 09:49:23 +0200

    fix center

--- neb/xyz_to_con.py
+++ neb/xyz_to_con.py
@@ -9,6 +9,7 @@ import readcon
 def main(xyz_path, con_path):
     atoms = read(xyz_path)
     atoms.set_cell([50,50,50])
+    atoms.translate(0.5 * atoms.cell.sum(axis=0) - atoms.get_center_of_mass())
     atoms.calc = None  # geometry only, no embedded forces
     readcon.ConFrame.from_ase(atoms).write_con(con_path)
 
